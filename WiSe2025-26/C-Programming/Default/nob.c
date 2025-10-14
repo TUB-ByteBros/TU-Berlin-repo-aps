f2500edd12341c2d699846807b1828d3106b9787
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-10-14 02:51:05 +0200

    .
    Signed-off-by: mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>

--- WiSe2025-26/C-Programming/Default/nob.c
+++ WiSe2025-26/C-Programming/Default/nob.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #define NOB_STRIP_PREFIX
 #define NOB_IMPLEMENTATION
@@ -15,6 +14,20 @@ typedef struct {
 Cmd cmd = {0};
 
 MY_Flags_args ARGS = {0};
+
+char *BUILD_DIR = "./build";
+char *SRC_DIR = "./src";
+char *BUILD_MAIN;
+
+void cmd_cc() {
+#ifdef _WIN32
+  cmd_append(&cmd, "clang");
+#else
+  cmd_append(&cmd, "cc");
+#endif
+
+  cmd_append(&cmd, "-std=c11", "-Wall", "-Wextra", "-Wpedantic", "-Werror");
+}
 
 void usage(FILE *stream) {
   fprintf(stream, "Usage: ./example [OPTIONS] [--] [ARGS]\n");
@@ -34,15 +47,20 @@ int main(int argc, char **argv) {
   bool run_build = false;
   bool rrun_build = false;
 
+  bool build = false;
+  bool bbuild = false;
+
+  bool clean = false;
+
   flag_bool_var(&help, "-help", false, "Print this help");
   flag_bool_var(&hhelp, "h", false, "Print this help");
   flag_bool_var(&rrun_build, "r", false, "Run build");
-  flag_bool_var(&run_build, "run_build", false, "Run build");
+  flag_bool_var(&run_build, "-run", false, "Run build");
+  flag_bool_var(&bbuild, "b", false, "Build");
+  flag_bool_var(&build, "-build", false, "Build");
+  flag_bool_var(&clean, "-clean", false, "Clean build directory");
 
-  cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", "main", "src/main.c");
-  if (!cmd_run(&cmd))
-    return 1;
-  cmd.count = 0;
+  BUILD_MAIN = nob_temp_sprintf("%s/main", BUILD_DIR);
 
   if (!flag_parse(ARGS.count, ARGS.items)) {
     usage(stderr);
@@ -53,15 +71,40 @@ int main(int argc, char **argv) {
   argc = flag_rest_argc();
   argv = flag_rest_argv();
 
-  if (help) {
+  if (help || hhelp) {
     usage(stdout);
     exit(0);
   }
+  if (build || bbuild) {
 
-  if (run_build || rrun_build) {
-    cmd_append(&cmd, "./main");
+    if (!nob_mkdir_if_not_exists(BUILD_DIR))
+      return 1;
+
+    cmd_cc();
+    cmd_append(&cmd, "-o", BUILD_MAIN);
+    cmd_append(&cmd, temp_sprintf("-I%s", SRC_DIR));
+    cmd_append(&cmd, temp_sprintf("%s/main.c", SRC_DIR));
     if (!cmd_run(&cmd))
       return 1;
+    cmd.count = 0;
+  }
+
+  if (run_build || rrun_build) {
+    cmd_append(&cmd, BUILD_MAIN);
+    if (!cmd_run(&cmd))
+      return 1;
+    cmd.count = 0;
+  }
+
+  if (clean) {
+    #ifdef _WIN32
+    cmd_append(&cmd, "rmdir", "/s", "/q", BUILD_DIR);
+    #else
+    cmd_append(&cmd, "rm", "-rf", BUILD_DIR);
+    #endif
+    if (!cmd_run(&cmd))
+      return 1;
+    cmd.count = 0;
   }
 
   return 0;
